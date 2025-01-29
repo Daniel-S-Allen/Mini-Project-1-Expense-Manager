0cfc9abe3d4157dc672a2f9dda3563538fe72b32
ramisa-z <ramisa.zaman@ontariotechu.net>
2025-01-28 22:43:10 -0500

    Update userinput.cpp

--- UserInput/userinput.cpp
+++ UserInput/userinput.cpp
@@ -1,57 +1,56 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 #include <string>
-
 
 
 int main(){
     int menu, choice;
     string name, option, currency, date;
     float cost;
-
-    cout << "--------------Main menu--------------" << endl; 
-    cout << "1. Edit Expenses" << endl;
-    cout << "2. View Expenses" << endl;
-
+    
+    
     do{
-        cout << "Enter a menu option: " << endl;
-        cin >> menu;
-    }while(menu>4 || menu<1);
+        cout << "--------------Main menu--------------" << endl; 
+        cout << "1. Edit Expenses" << endl;
+        cout << "2. View Expenses" << endl;
+        cout << "3. Exit " << endl;
 
-    //editing expenses
-    if(menu ==1){
-        cout << "1. Add expenses \n2. Delete expenses" << endl;
-        cin >> choice;
-        cout << "Enter name of expense" << endl;
-        cin >> name;
+        do{
+            cout << "Enter a menu option: " << endl;
+            cin >> menu;    
+        }while(menu>4 || menu<1);
 
-        //adding expenses
-        if(choice==1){
-            cout << "Enter cost of expense: " << endl;
-            cin >> cost;
+        //editing expenses
+        if(menu ==1){
+            cout << "1. Add expenses \n2. Delete expenses" << endl;
+            cin >> choice;
+            cout << "Enter name of expense" << endl;
+            cin >> name;
 
-            cout << "Enter currency: " << endl;
-            cin >> currency;
+            //adding expenses
+            if(choice==1){
+                cout << "Enter cost of expense: " << endl;
+                cin >> cost;
 
-            //saving date
-            cout << "Would you like to enter a date for the expense? (Enter yes or no): " << endl;
-            cin >> option;
-            if(option=="no"){
-                //result to default
+                cout << "Enter currency: " << endl;
+                cin >> currency;
+
+                //saving date
+                cout << "Would you like to enter a date for the expense? (Enter yes or no): " << endl;
+                cin >> option;
+                if(option=="no"){
+                    //result to default
+                }
+                else{
+                    cout << "Enter date: " << endl;
+                    cin >> date;
+                    //make sure date is formatted correctly
+                }
             }
-            else{
-                cout << "Enter date: " << endl;
-                cin >> date;
-                //make sure date is formatted correctly
-            }
+
         }
-
-        //saving expense
-
-
-
-    }
-
+    }while(menu!=3);
+    
+    
 }
