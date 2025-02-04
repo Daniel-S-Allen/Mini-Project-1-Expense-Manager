3833270ea1dab62f5ce4c3d6288131d85ec5f4f5
Nooh Alavi <noohalavi@gmail.com>
2025-02-04 08:53:25 -0500

    updated h file

--- Expense/ExpenseManager.h
+++ Expense/ExpenseManager.h
@@ -6,6 +6,6 @@ class ExpenseManager
 {
 public:
     std::vector<Expense> expenseList;
-    void addExpense(float amt, std::string curr = "CAD", std::string desc, std::string date) {}
+    void addExpense(float amt, std::string curr = "CAD", std::string desc="", std::string date="") {}
     void removeExpense() {}
 };
