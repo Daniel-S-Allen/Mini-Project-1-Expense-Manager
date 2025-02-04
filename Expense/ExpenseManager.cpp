ad74d8733a5ecc0d9efa5df1ec9588cf0f86ff28
Nooh Alavi <noohalavi@gmail.com>
2025-02-04 08:49:36 -0500

    updates

--- Expense/ExpenseManager.cpp
+++ Expense/ExpenseManager.cpp
@@ -9,13 +9,13 @@ private:
     std::vector<Expense> expenseList;
 
     bool isDateValid(std::string &date) {
-        std::regex dateRegex(R"(^\d{4}-\d{1,2}-\d{1,2}$)");
+        std::regex dateRegex(R"(^\d{4}-\d{1,2}-\d{1,2}$)"); // will ensure that date is in one of the following formats: "yyyy-mm-dd", "yyyy-m-dd", "yyyy-mm-d", "yyyy-m-d". Any other format is considered invalid & default date will be used.
         return std::regex_match(date, dateRegex);
     }
 public:
     void addExpense(float amt, std::string curr = "CAD", std::string desc="", std::string date="")
     {
-        // TODO: Check if date is invalid — format: "yyyy-mm-dd".
+        // Checks if date is invalid.
         if (isDateValid(date)) {
             this->expenseList.push_back(Expense(amt, curr, desc, date));
         }
