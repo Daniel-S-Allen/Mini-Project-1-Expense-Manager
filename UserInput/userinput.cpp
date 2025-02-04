b6a741edaa6073d74837cae32bd444c8d3c38d91
ramisa-z <ramisa.zaman@ontariotechu.net>
2025-02-04 09:24:16 -0500

    Update userinput.cpp

--- UserInput/userinput.cpp
+++ UserInput/userinput.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 #include "ExpenseManager.h"
 #include <string>
+#include <vector>
 
 int main(){
     int menu, choice;
-    string name, option, currency, date;
+    string name, option, currency, date, category;
     float cost;
     ExpenseManager manager;
     
     cout << "--------------Main menu--------------" << endl; 
     cout << "1. Add Expenses" << endl;
-    cout << "2. Edit Budgets" << endl;
+    cout << "2. Add Categories" << endl;
     cout << "3. View Expenses" << endl;
     
     do{
@@ -20,27 +21,32 @@ int main(){
         cin >> menu;    
     }while(menu>3 || menu<1);
     
-    //editing expenses
+    ////adding expenses
     if(menu ==1){
         cout << "Enter name of expense" << endl;
         cin >> name;
         
-        //adding expenses
         cout << "Enter cost of expense: " << endl;
         cin >> cost;
             
         cout << "Enter currency: " << endl;
         cin >> currency;
-            
-        //saving date
+        
         cout << "Enter date (yyyy-mm-dd): " << endl;
         cin >> date;
-    
         
-        manager.addExpense(cost, currency, name, date);
+        cout << "Enter category: " << endl;
+        cin >> category;
         
         //saving expense
-
+        manager.addExpense(cost, currency, name, date, category);    
+        
     }
+    
+    else if(menu==2){
+        cout << "Enter name of category: " << endl;
+        cin >> category;
+    }
+    
     
 }
