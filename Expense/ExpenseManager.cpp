a0ed1e48f0a2e1638d56731f221c906be9e8eeaa
Nooh Alavi <noohalavi@gmail.com>
2025-02-04 08:27:34 -0500

    idk what im doing

--- Expense/ExpenseManager.cpp
+++ Expense/ExpenseManager.cpp
@@ -4,9 +4,9 @@
 
 class ExpenseManager
 {
-public:
+private:
     std::vector<Expense> expenseList;
-
+public:
     void addExpense(int amt, std::string curr = "CAD", std::string desc="")
     {
         this->expenseList.push_back(Expense(amt, curr, desc));
