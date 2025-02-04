98b74f29542dc243f29da72b236099da9dbe900e
Nooh Alavi <noohalavi@gmail.com>
2025-02-04 09:03:54 -0500

    updated h file

--- Expense/ExpenseManager.cpp
+++ Expense/ExpenseManager.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <string>
 #include <regex>
+#include "database/file.h"
 
 class ExpenseManager
 {
 private:
     std::vector<Expense> expenseList;
+    std::vector<Category> categoryList;
 
     bool isDateValid(std::string &date) {
         std::regex dateRegex(R"(^\d{4}-\d{1,2}-\d{1,2}$)"); // will ensure that date is in one of the following formats: "yyyy-mm-dd", "yyyy-m-dd", "yyyy-mm-d", "yyyy-m-d". Any other format is considered invalid & default date will be used.
@@ -21,6 +23,10 @@ public:
         }
         else {
             this->expenseList.push_back(Expense(amt, curr, desc));
+        }
+
+        for (auto &category : categoryList) {
+            DatabaseIO::writeToFile("expenses.txt", category);
         }
     }
 
