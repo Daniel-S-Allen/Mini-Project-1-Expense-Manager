04bfed6d02fa5d89d554dfea72d538d1df1815fc
ramisa-z <ramisa.zaman@ontariotechu.net>
2025-02-04 23:22:34 -0500

    Update userinput.cpp

--- UserInput/userinput.cpp
+++ UserInput/userinput.cpp
@@ -11,42 +11,47 @@ int main(){
     float cost;
     ExpenseManager manager;
     
+    do{
     cout << "--------------Main menu--------------" << endl; 
     cout << "1. Add Expenses" << endl;
     cout << "2. Add Categories" << endl;
     cout << "3. View Expenses" << endl;
+    cout << "4. Exit" << endl;
     
-    do{
-        cout << "Enter a menu option: " << endl;
-        cin >> menu;    
-    }while(menu>3 || menu<1);
-    
-    ////adding expenses
-    if(menu ==1){
-        cout << "Enter name of expense" << endl;
-        cin >> name;
-        
-        cout << "Enter cost of expense: " << endl;
-        cin >> cost;
+        do{
+            cout << "Enter a menu option: " << endl;
+            cin >> menu;    
             
-        cout << "Enter currency: " << endl;
-        cin >> currency;
+        }while(menu>4 || menu<1);
+
+        ////adding expenses
+        if(menu ==1){
+            cout << "Enter name of expense" << endl;
+            cin >> name;
+
+            cout << "Enter cost of expense: " << endl;
+            cin >> cost;
+
+            cout << "Enter currency: " << endl;
+            cin >> currency;
+
+            cout << "Enter date (yyyy-mm-dd): " << endl;
+            cin >> date;
+
+            cout << "Enter category: " << endl;
+            cin >> category;
+
+            //saving expense
+            manager.addExpense(cost, currency, name, date, category);    
+
+        }
+
+        else if(menu==2){
+            cout << "Enter name of category: " << endl;
+            cin >> category;
+        }
         
-        cout << "Enter date (yyyy-mm-dd): " << endl;
-        cin >> date;
-        
-        cout << "Enter category: " << endl;
-        cin >> category;
-        
-        //saving expense
-        manager.addExpense(cost, currency, name, date, category);    
-        
-    }
-    
-    else if(menu==2){
-        cout << "Enter name of category: " << endl;
-        cin >> category;
-    }
+    }while(menu!=4);
     
     
 }
